83217e47425c6340b99f2ae5f5760b0827bf2c4b
BhodiJansen <399808@student.fontys.nl>
2024-05-19 12:47:22 +0200

    Delete + update team

--- TakenbeheerSysteem/TakenbeheerSysteem/Pages/Team/Delete.cshtml.cs
+++ TakenbeheerSysteem/TakenbeheerSysteem/Pages/Team/Delete.cshtml.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TakenbeheerCore.Employee;
+using TakenbeheerCore.Team;
 
 namespace TakenbeheerSysteem.Pages.Team
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private TeamService _service;
+		public void OnGet()
+		{
+
+		}
+
+		public DeleteModel(ITeamRepository irepo)
+		{
+			_service = new TeamService(irepo);
+		}
+
+
+		public ActionResult OnPost()
+		{
+			if (Request.Form["submit"] == "Delete")
+			{
+				try
+				{
+
+					_service.DeleteTeam(HttpContext.Session.GetInt32("deleteTarget") ?? default(int));
+				}
+				catch (Exception)
+				{
+					//Log what exactly went wrong and show custom error page
+				}
+				return Redirect("~/Index");
+			}
+			else
+			{
+				return Redirect("Details");
+			}
+		}
+	}
 }
--- TakenbeheerSysteem/TakenbeheerSysteem/Pages/Team/Edit.cshtml.cs
+++ TakenbeheerSysteem/TakenbeheerSysteem/Pages/Team/Edit.cshtml.cs
@@ -1,12 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TakenbeheerCore.Employee;
+using TakenbeheerCore.Team;
 
 namespace TakenbeheerSysteem.Pages.Team
 {
     public class EditModel : PageModel
     {
+        private TeamService _service;
+        public TeamModel input;
+
+        public EditModel(ITeamRepository repo)
+        {
+            _service = new TeamService(repo);
+        }
+
         public void OnGet()
         {
+            input = _service.GetTeamForEmployee(HttpContext.Session.GetInt32("uId") ?? default(int));
+        }
+
+
+        public ActionResult OnPost()
+        {
+            if (Request.Form["Submit"] == "Update")
+            {
+                TeamModel result = new TeamModel(
+                    input.Id,
+                    Request.Form["Name"],
+                    Request.Form["Address"],
+                    Request.Form["PostalCode"]
+                    );
+                if (_service.UpdateTeam(result))
+                {
+                    return Redirect("Details");
+                }
+                else
+                {
+                    return Redirect("Create");
+                }
+            }
+            else
+            {
+                return Redirect("Details");
+            }
         }
     }
 }
